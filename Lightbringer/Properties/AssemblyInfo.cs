aa92782b87ec01e32eb5e93f04c8ce55cec63900
Theodore Radu <theodorechristianradu@gmail.com>
2024-10-11 04:08:55 +0200

    Updated to Framework v4.8

--- Lightbringer/Properties/AssemblyInfo.cs
+++ Lightbringer/Properties/AssemblyInfo.cs
@@ -1,4 +1,17 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lightbringer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lightbringer")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -7,3 +20,16 @@
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ac3f861c-34e8-426e-bb02-390e53440529")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyFileVersion("3.0.1")]
